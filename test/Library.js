827f113574f121081017e0207302ae4e31d47343
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2022-10-26 11:14:04 +0200

    Updated tests

--- test/Library.js
+++ test/Library.js
@@ -12,6 +12,8 @@ describe("Library", () => {
     const Library = await ethers.getContractFactory("Library");
     const library = await Library.deploy();
 
+    await library.deployed();
+
     return { library, owner, otherAccount };
   }
 
@@ -308,68 +310,3 @@ describe("Library", () => {
     });
   });
 });
-
-// describe("Withdrawals", function () {
-//   describe("Validations", function () {
-//     it("Should revert with the right error if called too soon", async function () {
-//       const { lock } = await loadFixture(deployLibraryFixture);
-
-//       await expect(lock.withdraw()).to.be.revertedWith(
-//         "You can't withdraw yet"
-//       );
-//     });
-
-//     it("Should revert with the right error if called from another account", async function () {
-//       const { lock, unlockTime, otherAccount } = await loadFixture(
-//         deployLibraryFixture
-//       );
-
-//       // We can increase the time in Hardhat Network
-//       await time.increaseTo(unlockTime);
-
-//       // We use lock.connect() to send a transaction from another account
-//       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-//         "You aren't the owner"
-//       );
-//     });
-
-//     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-//       const { lock, unlockTime } = await loadFixture(deployLibraryFixture);
-
-//       // Transactions are sent using the first signer by default
-//       await time.increaseTo(unlockTime);
-
-//       await expect(lock.withdraw()).not.to.be.reverted;
-//     });
-//   });
-
-//   describe("Events", function () {
-//     it("Should emit an event on withdrawals", async function () {
-//       const { lock, unlockTime, lockedAmount } = await loadFixture(
-//         deployLibraryFixture
-//       );
-
-//       await time.increaseTo(unlockTime);
-
-//       await expect(lock.withdraw())
-//         .to.emit(lock, "Withdrawal")
-//         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-//     });
-//   });
-
-//   describe("Transfers", function () {
-//     it("Should transfer the funds to the owner", async function () {
-//       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-//         deployLibraryFixture
-//       );
-
-//       await time.increaseTo(unlockTime);
-
-//       await expect(lock.withdraw()).to.changeEtherBalances(
-//         [owner, lock],
-//         [lockedAmount, -lockedAmount]
-//       );
-//     });
-//   });
-// });
-// });
